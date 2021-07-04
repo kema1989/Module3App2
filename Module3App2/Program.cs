6275fb1e6087d5eafc0721abdf43780bd9552239
Кемран Абдрашид <kemran94875@gmail.com>
2021-07-04 13:02:19 +0600

    Добавьте файлы проекта.

---
+++ Module3App2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Module3App2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Kemran";
+            int age = 19;
+            bool pet = false;
+            double shoesize = 43.3;
+            Console.WriteLine("My name is " + name);
+            Console.WriteLine("I am " + age);
+            Console.WriteLine("Do I have a pet? " + pet);
+            Console.WriteLine("My shoesize is " + shoesize);
+            Console.ReadKey();
+        }
+    }
+}
